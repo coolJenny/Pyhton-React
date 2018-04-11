d243f1d46ed4b3a7be8d74adc945b4fc26abcf99
coolJenny <coolJenny009g@gmail.com>
2018-04-10 23:40:47 -0400

    ..

--- project/leads/urls.py
+++ project/leads/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 urlpatterns = [
-    path('api/lead/', views.LeadListCreate.as_view() ),
+    url('api/lead/', views.LeadListCreate.as_view() ),
 ]
